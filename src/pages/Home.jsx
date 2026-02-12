b234cfdb01ffcc265aafaf172fb3a6421ef07735
nicop-exe <109947815+nicop-exe@users.noreply.github.com>
2026-02-12 15:07:25 +0100

    Fix missing useContext import in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
 import { Music, TrendingUp, Radio } from 'lucide-react';
 
 const Home = () => {
